bdeee1777cd8d7b7f1aadf1959c3cc1b2f29e999
raulmahya <raulmahya11@gmail.com>
2023-10-10 00:03:27 +0700

    asdadsasd

---
+++ getfunction.js
@@ -0,0 +1,26 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
+
+export let URLGeoJson = "https://us-central1-testlogin-366704.cloudfunctions.net/function-7";
+export let tableTag="tr";
+export let tableRowClass="content is-small";
+export let tableTemplate=`
+<td>#TYPE#</td>
+<td>#NAME#</td>
+<td>#KORDINAT#</td>
+`
+
+export function responseData(results){
+    console.log(results);
+    results.forEach(isiRow);
+}
+
+export function isiRow(value) {
+    let content = tableTemplate
+        .replace("#TYPE#", value.geometry.type)
+        .replace("#NAME#", value.properties.name)
+        .replace("#KORDINAT#", JSON.stringify(value.geometry.coordinates));
+    console.log(content);
+    addChild("lokasi", tableTag, tableRowClass, content);
+}
+
